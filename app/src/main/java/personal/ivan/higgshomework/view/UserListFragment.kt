d92558d8ec9602c426dad962201428a9e2127b32
Ivan Kuo <ivanclair0710@gmail.com>
2020-06-04 13:42:59 +0800

    [s-4] create navigation graph and create fragment for first page

---
+++ app/src/main/java/personal/ivan/higgshomework/view/UserListFragment.kt
@@ -0,0 +1,37 @@
+package personal.ivan.higgshomework.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import personal.ivan.higgshomework.R
+import personal.ivan.higgshomework.databinding.FragmentUserListBinding
+
+class UserListFragment : Fragment() {
+
+    private lateinit var binding: FragmentUserListBinding
+
+    // region Life Cycle
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_user_list,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    // endregion
+}
