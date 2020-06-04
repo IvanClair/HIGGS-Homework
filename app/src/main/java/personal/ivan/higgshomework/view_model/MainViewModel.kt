cfb2c26931502d9a99ae3729f9f0f4c204758847
Ivan Kuo <ivanclair0710@gmail.com>
2020-06-04 19:36:06 +0800

    [s-5] create user list xml and view holder xml files

--- app/src/main/java/personal/ivan/higgshomework/view_model/MainViewModel.kt
+++ app/src/main/java/personal/ivan/higgshomework/view_model/MainViewModel.kt
@@ -1,8 +1,17 @@
 package personal.ivan.higgshomework.view_model
 
+import android.view.View
 import androidx.lifecycle.ViewModel
 import personal.ivan.higgshomework.repository.GitHubRepository
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(private val repository: GitHubRepository) : ViewModel() {
+
+    // region UI Event - User List
+
+    fun userOnClickListener(view: View) {
+
+    }
+
+    // endregion
 }
